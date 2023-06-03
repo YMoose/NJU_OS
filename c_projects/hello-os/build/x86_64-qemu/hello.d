11d051cf6bbe0e4566d9d242bbc24495d839c2d8
moose <1728093819@qq.com>
2023-06-03 08:38:46 +0000

    update with hello-os

---
+++ c_projects/hello-os/build/x86_64-qemu/hello.d
@@ -0,0 +1,5 @@
+/home/yzh/NJU_os/c_projects/hello-os/build/x86_64-qemu/hello.o: \
+ /home/yzh/NJU_os/c_projects/hello-os/hello.c \
+ /home/yzh/NJU_os/NJU-os-workbench/abstract-machine/am/include/am.h \
+ /home/yzh/NJU_os/NJU-os-workbench/abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/yzh/NJU_os/NJU-os-workbench/abstract-machine/am/include/amdev.h
